c7e1b0b5983c123c7cef4ba68174c92bb494456c
Simon Stensgaard <STcena1@hotmail.com>
2026-06-22 20:52:22 +0200

    Removed Firebase from Git

--- Firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyC2rE4QhzH_U24Ptp1hy2EYWN-v9vzEWFs",
-  authDomain: "dipexam-1fee5.firebaseapp.com",
-  projectId: "dipexam-1fee5",
-  storageBucket: "dipexam-1fee5.firebasestorage.app",
-  messagingSenderId: "205536275742",
-  appId: "1:205536275742:web:20ad940ce12dae8e9a2d0f",
-  databaseURL: "https://dipexam-1fee5-default-rtdb.europe-west1.firebasedatabase.app/"
-};
